f578b008a2075c2d432f2d05392ae0757137bbfa
VitorDani <vitord2002@hotmail.com>
2021-12-01 17:51:45 -0300

    Aula-2 blog pessoal

---
+++ BlogPessoal1/src/test/java/org/generation/BlogPessoal1/BlogPessoal1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.BlogPessoal1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPessoal1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
