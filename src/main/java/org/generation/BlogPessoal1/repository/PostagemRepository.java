e86cda52edb7f85f9c638ee7ad522f9d29048fa5
VitorDani <vitord2002@hotmail.com>
2021-12-11 13:36:35 -0300

    :pencil::memo: comentando o codigo

--- src/main/java/org/generation/BlogPessoal1/repository/PostagemRepository.java
+++ src/main/java/org/generation/BlogPessoal1/repository/PostagemRepository.java
@@ -6,7 +6,19 @@ import org.generation.BlogPessoal1.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/*
+ * 
+ * A annotation @Repository indica que a Classe é uma Repository, ou seja, é responsável 
+ * pela comunicação com o Banco de dados através dos métodos padrão e das Method Queries, 
+ * que são consultas personalizadas através de palavras chave que representam as instruções 
+ * da linguagem SQL
+ * 
+ */
+
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	/**
+	 * Esta method Query é equivalente a: select * from tb_postagem where titulo like "%titulo%";
+	 */
 	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
 }
