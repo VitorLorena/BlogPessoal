c43f1fddcbbd5b919bf8e8c5e2d2eb7e243dcc0c
VitorDani <vitord2002@hotmail.com>
2021-12-13 17:04:46 -0300

    :closed_lock_with_key: implementando as camadas de

---
+++ src/main/java/org/generation/BlogPessoal1/controller/UsuarioController.java
@@ -0,0 +1,50 @@
+package org.generation.BlogPessoal1.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.generation.BlogPessoal1.model.UserLogin;
+import org.generation.BlogPessoal1.model.Usuario;
+import org.generation.BlogPessoal1.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin("*")
+public class UsuarioController {
+	
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> getAll() {
+	
+		return ResponseEntity.ok(usuarioService.listarUsuarios());
+
+	}
+
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
+		return usuarioService.autenticarUsuario(user)
+				.map(respostaAutenticacao -> ResponseEntity.ok(respostaAutenticacao))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		return usuarioService.cadastrarUsuario(usuario)
+				.map(respostaCadastro -> ResponseEntity.status(HttpStatus.CREATED).body(respostaCadastro))
+				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+	}
+
+
+}
